6177a37022593ffa7350f06a4938e95f25f7dc75
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2022-04-21 09:00:08 -0400

    Log de si alguien muere por el AC

---
+++ RagnarokCore/Patches/ACNotifyPatch.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+using Exiled.API.Features;
+
+namespace RagnarokCore.Patches
+{
+    [HarmonyPatch(typeof(PlayerMovementSync), nameof(PlayerMovementSync.AntiCheatKillPlayer))]
+    public static class ACNotifyPatch
+    {
+        public static void Prefix(PlayerMovementSync __instance, string message, string code) => Log.Warn($"AntiCheatKill Detectado: {Player.Get(__instance._hub)?.Nickname} [{message}({code})]");
+    }
+}
